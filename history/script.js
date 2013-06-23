fcc9f1346dcd37e5704c2c041ddca0cf4b24e054
Martin Michálek <michalek@shortcat.cz>
2013-06-23 18:17:40 +0200

    History.JS example: handling ajax loads, loading indication when clicking on navigation anchors

--- history/script.js
+++ history/script.js
@@ -6,8 +6,23 @@
   var History = window.History; // Note: We are using a capital H instead of a lower h
 
   $('[role=navigation] a').click(function(e){
+    
+    // Active class
+    $('[role=navigation] a').removeClass('active');
+    $(this).addClass('active');
+    
+    // Loading indication + History.JS pushstate + ajax load 
+    $('[role=main]').addClass('loading');      
     History.pushState(null, null, $(this).attr('href'));
-    e.preventDefault();
+    $('[role=main]').load(
+      $(this).attr('href') + ' [role=main]',
+      function() {
+        $('[role=main]').removeClass('loading');
+      }
+    );
+    
+    // Prevent default click behavior
+    e.preventDefault();    
   })
 
   History.Adapter.bind(window,'statechange',function(){ 
