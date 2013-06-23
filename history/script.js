76043e0071091ddaaba92d4814b47c20d3f99d22
Martin Michálek <michalek@shortcat.cz>
2013-06-23 19:05:07 +0200

    History.JS example: ajax loads on back/forward buttons too - we have to use absolute url paths

--- history/script.js
+++ history/script.js
@@ -2,32 +2,36 @@
 
 (function(window){
 
-  // Inicializovat History.JS
+  // Initialize History.JS
   var History = window.History; // Note: We are using a capital H instead of a lower h
 
-  $('[role=navigation] a').click(function(e){
-    
-    // Active class
-    $('[role=navigation] a').removeClass('active');
-    $(this).addClass('active');
-    
-    // Loading indication + History.JS pushstate + ajax load 
-    $('[role=main]').addClass('loading');      
+  // Clicking on navigation items
+  $('[role=navigation] a').click(function(e){    
     History.pushState(null, null, $(this).attr('href'));
-    $('[role=main]').load(
-      $(this).attr('href') + ' [role=main]',
-      function() {
-        $('[role=main]').removeClass('loading');
-      }
-    );
-    
-    // Prevent default click behavior
     e.preventDefault();    
   })
 
+  // Listening 'statechange' event: on pushState and click on back or forward buttons
   History.Adapter.bind(window,'statechange',function(){ 
     var State = History.getState(); 
-    History.log(State.data, State.title, State.url);
+    History.log(State.data, State.title, State.url);        
+    ContentLoad(State.url);
   });  
+  
+  
+  // Content loading function
+  function ContentLoad(url) {    
+    // Active class on navigation
+    $('[role=navigation] a').removeClass('active');
+    $('[role=navigation] a[href="'+url+'"]').addClass('active');
+    // Loading indication + History.JS pushstate + ajax load 
+    $('[role=main]').addClass('loading');      
+    $('[role=main]').load(
+      url + ' [role=main]',
+      function() {
+        $('[role=main]').removeClass('loading');
+      }
+    );    
+  }
 
 })(window);
